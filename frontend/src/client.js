916471d836072d34894e3e140e0a2fed3585c1c4
EpicIbby101 <kevin_grittner@outlook.com>
2023-03-06 16:20:28 +0000

    Changed useCDN to false in client.js

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -5,7 +5,7 @@ export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2023-01-12",
-  useCdn: true,
+  useCdn: false,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
